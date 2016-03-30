98a8c36e2d6083d1367309255bff1a20bc3c5613
greentea524 <cpdawei@gmail.com>
2016-03-30 00:08:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('#myButton').click(function(){
-		$('img').fadeTo('slow',0.5);
+		$('img').fadeTo('fast',1);
 	});
 	$('#rightButton').click(function(){
 		$('img').animate({left: "+=10px"}, 'fast');
@@ -9,11 +9,14 @@ $(document).ready(function() {
 		$('img').animate({left: "-=10px"}, 'fast');
 	});
 	$('img').mouseenter(function(){
-		$('img').fadeTo('fast',1);
-	});
-	$('img').mouseleave(function(){
 		$('img').fadeTo('fast',0.5);
 	});
+	$('img').mouseleave(function(){
+		$('img').fadeTo('fast',1);
+	});
+	$('img').click(function(){
+		$('img').fadeOut('slow');
+	})
     $(document).keydown(function(key) {
         switch(parseInt(key.which,10)) {
             
