0d03c860bd9e42a8f200df19c080acc1dee2c6cf
greentea524 <cpdawei@gmail.com>
2016-03-29 23:01:00 -0700

    mario animate
    add script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $(document).keydown(function(key) {
+        var mario = $('img');
+        switch(parseInt(key.which,10)) {
+            
+			// Left arrow key pressed
+			case 37:
+				mario.animate({left: "-=10px"}, 'fast');
+				break;
+			// Up Arrow Pressed
+			case 38:
+				mario.animate({top: "-=10px"}, 'fast');
+				break;
+			// Right Arrow Pressed
+			case 39:
+				mario.animate({left: "+=10px"}, 'fast');
+				break;
+			// Down Arrow Pressed
+			case 40:
+				mario.animate({top: "+=10px"}, 'fast');
+				break;
+		}
+	});
+});
