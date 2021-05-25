0081715b35bbfe696747850880f85f3a64410628
greentea524 <cpdawei@gmail.com>
2021-05-24 18:23:33 -0700

    DataTable
    display DataTable with Mockend data

---
+++ js/display_data.js
@@ -0,0 +1,72 @@
+
+const PlayData = function() {
+
+    function jsonPlaceHolder() {
+        displayDataTableUser();
+    }
+
+    function fetchMockend() {
+        displayDataTablePost();
+    }
+
+	return {
+		jsonPlaceHolder: jsonPlaceHolder,
+		fetchMockend: fetchMockend
+	}
+
+}();
+
+PlayData.jsonPlaceHolder();
+PlayData.fetchMockend();
+
+function displayDataTableUser() {
+
+    $('#myTableOne').DataTable({
+        ajax: {
+            url: "https://jsonplaceholder.typicode.com/users",
+            type: "GET",
+            dataSrc: ""
+        },
+        columns: [
+            { "data": "id" },
+            { "data": "name" },
+            { "data": "username" },
+            { "data": "email" },
+            { "data": "website" }
+        ],
+    });
+
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // .then(response => response.json())
+    // .then(json => console.log(json))
+}
+function displayDataTablePost() {
+
+    $('#myTableTwo').DataTable({
+        ajax: {
+            url: "https://mockend.com/greentea524/greentea524.github.io/posts",
+            type: "GET",
+            dataSrc: ""
+        },
+        columns: [
+            { "data": "title" },
+            { "data": "views" },
+            { "data": "published" },
+            { "data": "createdAt" }
+        ],
+    });
+
+    // fetch('https://mockend.com/greentea524/greentea524.github.io/posts')
+    // .then(response => response.json())
+    // .then(json => {
+    //     console.log(json)
+    //     //displayDataTablePost(json);
+    // })
+    //
+    // fetch('https://mockend.com/greentea524/greentea524.github.io/users/1')
+    // .then(response => response.json())
+    // .then(json => {
+    //     console.log(json)
+    // })
+
+}
